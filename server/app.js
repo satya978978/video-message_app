c10991bdab79db57188822ed74fed9270803d03a
Satya Yadav <satyaxyadavvv@gmail.com>
2025-08-04 03:50:14 +0530

    cors fix

--- server/app.js
+++ server/app.js
@@ -85,7 +85,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONT_END,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
